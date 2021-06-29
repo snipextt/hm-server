6402bb09548cda733fecf9ecf07a72420639ea39
snipextt <snipextt@gmail.com>
2021-06-29 13:19:05 +0530

    Add patient and doctor routes

--- src/routers/DoctorRouter.ts
+++ src/routers/DoctorRouter.ts
@@ -1,7 +1,36 @@
 import { Router } from 'express';
+import { Appointment, Prescription } from '../db';
 import { DoctorMiddlewere } from './auth';
 
 const DoctorRouter = Router();
 DoctorRouter.use(DoctorMiddlewere);
+
+DoctorRouter.get('/appointments/:id', async (req, res) => {
+  const id = req.params.id;
+  if (!id) res.status(400).json({ err: 'Missing id' });
+  const appointments = await Appointment.find({ doctor: id }).catch((err) => {
+    res.status(500).json(err);
+  });
+  if (appointments!.length) res.status(200).json(appointments);
+});
+
+DoctorRouter.get('/prescriptions/:id', async (req, res) => {
+  const id = req.params.id;
+  if (!id) res.status(400).json({ err: 'Missing id' });
+  const prescription = await Prescription.find({ doctor: id }).catch((err) => {
+    res.status(500).json(err);
+  });
+  if (prescription) res.status(200).json(prescription);
+});
+
+DoctorRouter.post('/prescription', async (req, res) => {
+  const presecriptionInfo = req.body;
+  const prescription = await Prescription.create({
+    ...presecriptionInfo,
+  }).catch((err: any) => {
+    res.status(400).json({ err: err });
+  });
+  if (prescription) res.status(200).json(prescription);
+});
 
 export default DoctorRouter;
--- src/routers/PatientRouter.ts
+++ src/routers/PatientRouter.ts
@@ -1,7 +1,43 @@
 import { Router } from 'express';
+import { Appointment, Prescription, User } from '../db';
 import { PatientMiddlewere } from './auth';
 
 const PatientRouter = Router();
 PatientRouter.use(PatientMiddlewere);
+
+PatientRouter.get('/appointments/:id', async (req, res) => {
+  const id = req.params.id;
+  if (!id) res.status(400).json({ err: 'Missing id' });
+  const appointments = await Appointment.find({ patient: id }).catch((err) => {
+    res.status(500).json(err);
+  });
+  if (appointments!.length) res.status(200).json(appointments);
+});
+
+PatientRouter.get('/prescriptions/:id', async (req, res) => {
+  const id = req.params.id;
+  if (!id) res.status(400).json({ err: 'Missing id' });
+  const appointments = await Prescription.find({ patient: id }).catch((err) => {
+    res.status(500).json(err);
+  });
+  if (appointments!.length) res.status(200).json(appointments);
+});
+
+PatientRouter.post('/appointment', async (req, res) => {
+  const appointmentInfo = req.body;
+  const appointment = await Appointment.create({
+    ...appointmentInfo,
+  }).catch((err: any) => {
+    res.status(400).json({ err: err });
+  });
+  if (appointment) res.status(200).json(appointment);
+});
+
+PatientRouter.get('/doctors', async (req, res) => {
+  const doctors = await User.find({ userType: 'd' }).catch((err) => {
+    res.status(400).json({ err: err });
+  });
+  if (doctors!.length) res.status(200).json(doctors);
+});
 
 export default PatientRouter;
