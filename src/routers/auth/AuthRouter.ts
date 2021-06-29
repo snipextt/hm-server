bc34e89b32eb65c778363a7ab91a00e89de39ea3
snipextt <snipextt@gmail.com>
2021-06-29 19:13:18 +0530

    Add user id

--- src/routers/auth/AuthRouter.ts
+++ src/routers/auth/AuthRouter.ts
@@ -26,7 +26,7 @@ AuthRouter.post('/login', async (req, res) => {
     'key',
     (err: Error | null, authToken: string | undefined) => {
       if (err) res.status(500).json(err);
-      else res.status(200).json({ authToken });
+      else res.status(200).json({ authToken, userID: user.id });
     }
   );
 });
